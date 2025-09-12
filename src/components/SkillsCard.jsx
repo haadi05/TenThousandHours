fa89064548b1ccf00839aefb0b0887f0fd67706b
hadi <hadishamsi854@gmail.com>
2025-09-12 14:19:51 +0500

    feat: implement context and daily hours logging

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,16 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import SkillContext from "../context/Context";
 
-function SkillCard({ color }) {
-  const { skill } = useContext(SkillContext);
+function SkillCard() {
+  const { skill, setSkill } = useContext(SkillContext);
+
+  const [inputHours, setInputHours] = useState("");
+  const [showInput, setShowInput] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSkill((prev) => ({
+      ...prev,
+      inputHours,
+    }));
+  };
+
   const title = skill.skillName;
-  const days = 1;
-  const hour = skill.hours;
-  const remaining = skill.hours;
-  const dailyAvg = 7;
+  const hours = skill.hours;
+  const logged_hours = skill.inputHours;
+  const remaining = hours - logged_hours;
+  const color = skill.color;
+  const percentage = (logged_hours * 100) / hours;
+  const fillWidth = (logged_hours * 315) / hours;
 
   return (
-    <div className="w-100 mt-5 p-6 rounded-lg shadow-md  bg-[#1e232d]">
+    <div className="w-100 mt-5 p-6 rounded-lg shadow-md bg-[#1e232d]">
       <div className="flex justify-between items-center mb-4">
         <h3 className="font-semibold text-lg">{title}</h3>
         <div
@@ -26,32 +40,56 @@ function SkillCard({ color }) {
       </div>
 
       <div className="flex justify-center items-center mb-6">
-        <div className="w-full bg-[#2b313f] rounded-4xl h-3 overflow-hidden">
+        <div className="w-full bg-[#2b313f] rounded-4xl h-3 overflow-hidden ">
           <div
-            className={"h-full  w-1/3 flex items-center"}
-            style={{ backgroundColor: `#${color}` }}
+            className={`h-full flex items-center`}
+            style={{ backgroundColor: `#${color}`, width: `${fillWidth}px` }}
           ></div>
         </div>
 
-        <p className="text-sm m-0 pl-2 text-white">26%</p>
+        <p className="text-sm m-0 pl-2 text-white">{percentage}%</p>
       </div>
       <div className="flex justify-between text-sm mb-2">
         <div>
-          <span className="text-gray-400">Goal: </span> {hour}h
+          <span className="text-gray-400">logged Hours: </span> {logged_hours}h
         </div>
         <div>
           <span className="text-gray-400">Remaining:</span> {remaining}h
         </div>
       </div>
 
-      <button
-        className={
-          "cursor-pointer w-full text-[16px] bg py-2 rounded-md font-semibold"
-        }
-        style={{ backgroundColor: `#${color}` }}
-      >
-        Log Hours
-      </button>
+      {showInput ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            max={10000}
+            value={inputHours}
+            onChange={(e) => setInputHours(e.target.value)}
+            type="number"
+            className="w-full p-2 rounded-md bg-[#2b313f] outline-0 border border-gray-600 text-sm mb-4"
+          />
+          <button
+            type="submit"
+            className={
+              "cursor-pointer w-full text-[16px] bg py-2 rounded-md font-semibold"
+            }
+            style={{ backgroundColor: `#${color}` }}
+          >
+            Log Hours
+          </button>
+        </form>
+      ) : null}
+
+      {showInput ? null : (
+        <button
+          onClick={() => setShowInput(true)}
+          className={
+            "cursor-pointer w-full text-[16px] bg py-2 rounded-md font-semibold"
+          }
+          style={{ backgroundColor: `#${color}` }}
+        >
+          Log Hours
+        </button>
+      )}
     </div>
   );
 }
