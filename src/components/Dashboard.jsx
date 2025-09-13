acaf0aea1926999cad6dfa3e4121af134d583c0d
hadi <hadishamsi854@gmail.com>
2025-09-14 00:28:37 +0500

    chore: minor cleanup

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,12 @@
-import { SkillsCard, Heatmap } from "../index.js";
+import { SkillsCard, NewHeatMap } from "../index.js";
 
 function Dashboard() {
   return (
     <>
       <p className="ml-18 mt-4 text-2xl text-gray-400">Your Skills</p>
       <div className="flex mx-18 gap-4">
-        <div>
-          <SkillsCard />
-        </div>
-        <div className="w-full">
-          <Heatmap />
-        </div>
+        <SkillsCard />
+        <NewHeatMap color={"#fff"} />
       </div>
     </>
   );
