ef0a99a9aceab5a2b079226f832aa892ccafcfa7
hadi <hadishamsi854@gmail.com>
2025-09-14 16:04:21 +0500

    feat: global theme file

---
+++ src/themes/theme.js
@@ -0,0 +1,26 @@
+export const themes = {
+  blue: {
+    shade1: "#1a2633", // very dark
+    shade2: "#2f4d70", // dark-medium
+    shade3: "#4f77a8", // medium
+    shade4: "#90bae1", // lightest
+  },
+  purple: {
+    shade1: "#2b1d2c",
+    shade2: "#5a3f5a",
+    shade3: "#9b6e9b",
+    shade4: "#d8b4d8",
+  },
+  teal: {
+    shade1: "#1e3433",
+    shade2: "#3e6a68",
+    shade3: "#5ba3a0",
+    shade4: "#a0dad7",
+  },
+  orange: {
+    shade1: "#2f1a12",
+    shade2: "#703c2d",
+    shade3: "#d08770",
+    shade4: "#f4c3b0",
+  },
+};
