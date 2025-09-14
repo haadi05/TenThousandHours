7b13571385ee90054934dfdf699aeb6fda909334
hadi <hadishamsi854@gmail.com>
2025-09-14 16:03:08 +0500

    chore: cleanups

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { default as SkillsCard } from "./components/SkillsCard";
 export { default as Dashboard } from "./components/Dashboard";
-export { default as Heatmap } from "./components/Heatmap";
 export { default as AddSkills } from "./components/AddSkills";
 export { default as Navbar } from "./components/Navbar";
-export { default as NewHeatMap } from "./components/NewHeatMap";
+export { default as HeatmapGraph } from "./components/HeatmapGraph";
