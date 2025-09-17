6787795d60d0296a635f221b9dd7637c96909113
hadi <hadishamsi854@gmail.com>
2025-09-17 22:45:11 +0500

    feat: login component add + auth implemented

---
+++ src/components/Login.jsx
@@ -0,0 +1,146 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Navigate, NavLink } from "react-router-dom";
+// Firebase Imports
+import {
+  doSignInWithEmailAndPassword,
+  doSignInWithGoogle,
+} from "../firebase/auth";
+import { useAuth } from "../context/AuthContext";
+
+function LoginForm({ className, ...props }) {
+  const { userLoggedIn } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setErrorMessage("Please fill in all fields");
+      return;
+    }
+
+    if (!isSigningIn) {
+      try {
+        setIsSigningIn(true);
+        await doSignInWithEmailAndPassword(email, password);
+      } catch (error) {
+        setErrorMessage(error.Message);
+        setIsSigningIn(false);
+      }
+    }
+  };
+  const onGoogleSignIn = async (e) => {
+    e.preventDefault();
+    if (!isSigningIn) {
+      try {
+        setIsSigningIn(true);
+        doSignInWithGoogle();
+      } catch (error) {
+        setErrorMessage(error.Message);
+        setIsSigningIn(false);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        {userLoggedIn && <Navigate to={"/dashboard"} replace />}
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+          <Card className="w-sm">
+            <p className="flex justify-center font-semibold text-2xl ">
+              Login to your account
+            </p>
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-3">
+                    <div className="flex justify-between">
+                      <Label htmlFor="email">Email</Label>
+                      <div className="p-1 text-sm font-medium text-red-500">
+                        {errorMessage}
+                      </div>
+                    </div>
+                    <Input
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                      id="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                      <a
+                        href="#"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                    <Input
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                      id="password"
+                      type="password"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <Button
+                      disabled={isSigningIn}
+                      onClick={(e) => handleSubmit(e)}
+                      type="submit"
+                      className="w-full cursor-pointer"
+                    >
+                      Login
+                    </Button>
+                    <Button
+                      disabled={isSigningIn}
+                      onClick={(e) => onGoogleSignIn(e)}
+                      variant="outline"
+                      className="w-full cursor-pointer"
+                    >
+                      <img
+                        src="./src/assets/googleLogo.svg"
+                        className="size-5"
+                      />
+                      Continue with Google
+                    </Button>
+                  </div>
+                </div>
+                <div className="mt-4 text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <NavLink
+                    to={"/signup"}
+                    // This replaces the path with /signup
+                    replace
+                    className="underline underline-offset-4"
+                  >
+                    Sign up
+                  </NavLink>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginForm;
