579d605895bc19757f6cbee5b9fb6c5ac34376b5
hadi <hadishamsi854@gmail.com>
2025-09-17 22:47:36 +0500

    feat: user profile menu + signout option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { doSignOut } from "../firebase/auth.js";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
   return (
     <nav className="bg-[#161b22] text-white px-18 py-4 flex justify-between items-center">
       <div>
@@ -10,12 +21,26 @@ function Navbar() {
         </p>
       </div>
       <div className="flex items-center gap-3">
-        <NavLink
-          to={"/addskills"}
-          className="cursor-pointer text-black bg-gray-100 px-3 py-1 text-[16px] rounded-md"
-        >
-          + Add Skill
-        </NavLink>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild className="rounded-full">
+            <img
+              src="https://api.dicebear.com/9.x/glass/svg?seed=Felix"
+              className="cursor-pointer size-10"
+            />
+          </PopoverTrigger>
+          <PopoverContent className="w-fit p-1 m-0">
+            <button
+              onClick={() => {
+                doSignOut().then(() => {
+                  navigate("/login");
+                });
+              }}
+              className="outline-none cursor-pointer hover:bg-accent rounded-sm px-2 py-1"
+            >
+              Signout
+            </button>
+          </PopoverContent>
+        </Popover>
       </div>
     </nav>
   );
