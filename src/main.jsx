794f4a0a548bcad2094aa3bb679ca1fd4ac98b83
hadi <hadishamsi854@gmail.com>
2025-09-17 22:39:51 +0500

    feat: added login/signup routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,31 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Dashboard, AddSkills } from "./index.js";
+import { Dashboard, AddSkills, LoginForm, SignupForm, Home } from "./index.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <Dashboard /> },
       {
-        path: "addskills",
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <LoginForm />,
+      },
+      {
+        path: "signup",
+        element: <SignupForm />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "dashboard/addskills",
         element: (
           <>
             <Dashboard />
