a39a8d1f4f6368abc643a3836ee242da575acc41
hadi <hadishamsi854@gmail.com>
2025-09-17 22:41:51 +0500

    feat: updated ui layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,29 @@
 import { SkillsCard, HeatmapGraph } from "../index.js";
 import SkillContext from "../context/Context.js";
 import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
 function Dashboard() {
   const { skill } = useContext(SkillContext);
 
   return (
-    <>
-      <p className="ml-18 mt-4 text-2xl text-gray-400">Your Skills</p>
+    <div className="flex flex-col mx-18 mt-4">
+      <div className="flex justify-between items-center">
+        <p className="text-2xl text-gray-400">Your Skills</p>
+        <NavLink
+          to={"addskills"}
+          className="cursor-pointer text-black bg-gray-100 px-3 py-1 text-[16px] rounded-md"
+        >
+          + Add Skill
+        </NavLink>
+      </div>
       {skill.map((skill) => (
-        <div className="flex mx-18 gap-4" key={skill.id}>
+        <div className="flex gap-4" key={skill.id}>
           <SkillsCard skillsList={skill} />
           <HeatmapGraph skillsList={skill} />
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
