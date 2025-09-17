dc623702cab54cbd4f23e8a29bbc1ffdf9c435b9
hadi <hadishamsi854@gmail.com>
2025-09-17 22:38:26 +0500

    chore: updated paths

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { default as Dashboard } from "./components/Dashboard";
 export { default as AddSkills } from "./components/AddSkills";
 export { default as Navbar } from "./components/Navbar";
 export { default as HeatmapGraph } from "./components/HeatmapGraph";
+export { default as Home } from "./components/Home";
+export { default as LoginForm } from "./components/Login";
+export { default as SignupForm } from "./components/Signup";
