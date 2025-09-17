b6701d884a25111cd0b33ad7c1f15e9f0bbe3b1d
hadi <hadishamsi854@gmail.com>
2025-09-17 22:48:59 +0500

    feat: auth context added

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // listener function (listens for events like sign in & sign out  )
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+    setLoading(false);
+  }
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {/* if hook is not loading user  */}
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
