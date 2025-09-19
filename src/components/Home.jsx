4d34f296c2a61eb2914d6864df9794a06ec84e64
hadi <hadishamsi854@gmail.com>
2025-09-19 23:00:21 +0500

    feat: home page added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <p>Home will be added soon</p>
+      <div className="flex flex-col justify-center items-center mt-30">
+        <img src="./src/assets/target.svg" className="size-16" />
+        <p className="text-2xl mt-1">Start your Journey to Mastery</p>
+        <NavLink
+          to={"/dashboard/addskills"}
+          className="cursor-pointer text-black bg-gray-100 px-3 py-1 mt-3 text-lg font-medium rounded-md"
+        >
+          Get Started
+        </NavLink>
+      </div>
     </>
   );
 }
