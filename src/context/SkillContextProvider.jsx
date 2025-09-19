5886d464b60e3cfd768ad3fb795e166097bba280
hadi <hadishamsi854@gmail.com>
2025-09-19 23:06:17 +0500

    feat: firebase fetch data setup

--- src/context/SkillContextProvider.jsx
+++ src/context/SkillContextProvider.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SkillContext from "./Context";
+//Firebase imports
+import { useAuth } from "./AuthContext";
+import { getData } from "../firebase/db";
 
 function SkillContextProvider({ children }) {
   const [skill, setSkill] = useState([]);
+
+  const { currentUser } = useAuth();
+
+  //Optional Chaining
+  const userId = currentUser?.uid;
+
+  useEffect(() => {
+    if (!userId) {
+      setSkill([]);
+      return;
+    }
+
+    getData(userId).then(setSkill);
+  }, [userId]);
+
   return (
     <SkillContext.Provider value={{ skill, setSkill }}>
       {children}
