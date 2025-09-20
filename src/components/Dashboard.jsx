5209bc3560ff5368d0ed5da8e5901aac58c1772d
hadi <hadishamsi854@gmail.com>
2025-09-20 12:55:22 +0500

    feat: loader logic implemented

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,23 @@
 import { SkillsCard, HeatmapGraph } from "../index.js";
 import SkillContext from "../context/Context.js";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 function Dashboard() {
-  const { skill } = useContext(SkillContext);
+  const { loading, skill } = useContext(SkillContext);
+  const [minLoadingTime, setMinLoadingTime] = useState(true);
+
+  useEffect(() => {
+    // Set minimum loading time of 1 second
+    const timer = setTimeout(() => {
+      setMinLoadingTime(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="flex flex-col mx-18 mt-4">
+    <div className="flex flex-col mx-18 mt-4 max-[1320px]:mx-12 max-[860px]:mx-8 max-[630px]:mx-4">
       <div className="flex justify-between items-center">
         <p className="text-2xl text-gray-400">Your Skills</p>
         <NavLink
@@ -17,12 +27,18 @@ function Dashboard() {
           + Add Skill
         </NavLink>
       </div>
-      {skill.map((skill) => (
-        <div className="flex gap-4" key={skill.id}>
-          <SkillsCard skillsList={skill} />
-          <HeatmapGraph skillsList={skill} />
+      {loading || minLoadingTime ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="loader"></div>
         </div>
-      ))}
+      ) : (
+        skill.map((skill) => (
+          <div className="flex max-[1676px]:flex-col gap-4" key={skill.id}>
+            <SkillsCard skillsList={skill} />
+            <HeatmapGraph skillsList={skill} />
+          </div>
+        ))
+      )}
     </div>
   );
 }
